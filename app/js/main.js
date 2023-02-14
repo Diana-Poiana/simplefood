ccbeef1e958aeabfee2aec168762f4d62bee1645
Diana-Poiana <30.01.dxdy@gmail.com>
2023-02-14 20:59:18 +0100

    homework number 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,24 @@
+
+
+
 $(function () {
+
   $("#submit").hide();
   $("#search").on('click', function () {
     $("#submit").show();
     $("#search").hide();
   })
-});
-
-$(function () {
 
   $('.search__btn, .search__submit-btn').on('click', function () {
     $('.search').toggleClass('search--active');
   });
 
+
   $('.review__content').slick({
     dots: true,
-    fade: true
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-prev" width="34" height="34"><use href="images/sprite.svg#next-arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="slick-next" width="34" height="34"><use href="images/sprite.svg#prev-arrow"></use></svg></button>',
   });
 
   $('.categories__filter-btn').on('click', function () {
