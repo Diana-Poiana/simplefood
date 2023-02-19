de4bfce82c2db0a63dc39f551f9e3a2b7f94cc86
Diana-Poiana <30.01.dxdy@gmail.com>
2023-02-19 12:47:21 +0100

    homework 14

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,80 @@
 
+$(document).ready(function () {
+  //CHECKBOX
+
+  $.each($('.filter-offer__label'), function (index, val) {
+    if ($(this).find('input').prop('checked') == true) {
+      $(this).addClass('active');
+    }
+  });
+
+  $(document).on('click', '.filter-offer__label', function (event) {
+    if ($(this).hasClass('active')) {
+      $(this).find('input').prop('checked', false);
+    } else {
+      $(this).find('input').prop('checked', true);
+    }
+    $(this).toggleClass('active');
+
+    return false;
+  });
+});
 
 
 $(function () {
 
-  $("#submit").hide();
-  $("#search").on('click', function () {
-    $("#submit").show();
-    $("#search").hide();
-  })
+  //ION-SLIDER
 
-  $('.search__btn, .search__submit-btn').on('click', function () {
-    $('.search').toggleClass('search--active');
+  $('.filter-price__input').ionRangeSlider({
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
   });
 
+  //FORM STYLER
 
-  $('.review__content').slick({
+  $('.product-catalog__select').styler();
+
+  //MENU BUTTONS
+
+  $('.search__btn').on('click', function () {
+    $('.search__form').toggleClass('active');
+  });
+
+  $('.search__submit-btn').on('click', function () {
+    $('.search__form').toggleClass('active');
+  });
+
+  //SLICK SLIDER
+
+  $('.review__inner').slick({
     dots: true,
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-prev" width="34" height="34"><use href="images/sprite.svg#next-arrow"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="slick-next" width="34" height="34"><use href="images/sprite.svg#prev-arrow"></use></svg></button>',
+    accessibility: true,
+    appendArrows: '.slick-nav',
+    appendDots: '.slick-nav__dots',
+    prevArrow: $('.slick-nav__prev'),
+    nextArrow: $('.slick-nav__next'),
   });
+
+  //CATEGORIES FILTER BUTTONS
 
   $('.categories__filter-btn').on('click', function () {
     $('.this').addClass('categories__filter-btn--active')
     $('.categories__filter-btn').removeClass('categories__filter-btn--active');
   });
 
+
   var mixer = mixitup('.categories__product');
+
+});
+
+$(function () {
+
+  var mixer = mixitup('.product-catalog__list');
 
 });
